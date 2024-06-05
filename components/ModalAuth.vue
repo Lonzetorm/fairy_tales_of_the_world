c1f0fd08550134476878fd7c388cc0fa0e727807
Азаров Владимир <azarov@solid.com.ru>
2024-06-06 00:21:25 +0300

    Авторизация

--- components/ModalAuth.vue
+++ components/ModalAuth.vue
@@ -20,12 +20,12 @@
           <div class="p-4 md:p-5">
             <form class="space-y-4" action="#">
               <div>
-                <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Ваш логин/email</label>
-                <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name@company.com" required />
+                <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Ваш email</label>
+                <input v-model="email" type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name@company.com" required />
               </div>
               <div>
                 <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Ваш пароль</label>
-                <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required />
+                <input v-model="password" type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required />
               </div>
               <div class="flex justify-between">
                 <div class="flex items-start">
@@ -36,7 +36,14 @@
                 </div>
                 <a href="#" class="text-sm text-blue-700 hover:underline dark:text-blue-500">Забыли пароль?</a>
               </div>
-              <button type="submit" class="w-full text-white bg-green-600 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Войти</button>
+              <div v-if="errors" class="text-red-400">
+                <ul class="block sm:inline">
+                  <li v-for="[key, value] in errors">
+                    {{ value.message }}
+                  </li>
+                </ul>
+              </div>
+              <button type="submit" @click.prevent="postLoginForm" class="w-full text-white bg-green-600 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Войти</button>
               <div class="text-sm font-medium text-gray-500 dark:text-gray-300">
                 Не зарегистрированы? <span @click="toggleModal" class="text-blue-700 hover:underline dark:text-blue-500 cursor-pointer">Создать аккаунт</span>
               </div>
@@ -49,7 +56,20 @@
 </template>
 
 <script setup lang="ts">
+import { ref } from "@vue/reactivity";
+import { loginWithEmail } from "~/composables/useAuth";
+import type { Ref } from "vue"
+
 const store = useMainStore()
+const email = ref('')
+const password = ref('')
+
+const errors: Ref<Map<string, { message: InputValidation; }> | undefined> = ref(new Map<string, { message: InputValidation }>())
+let response: FormValidation
+
+// definePageMeta({
+//   middleware: 'guest'
+// })
 
 function toggleModal() {
   close()
@@ -60,6 +80,9 @@ function close() {
   store.modalAuthCall = false
 }
 
-
+async function postLoginForm() {
+  response = await loginWithEmail(email.value, password.value)
+  errors.value = response.errors
+}
 
 </script>
