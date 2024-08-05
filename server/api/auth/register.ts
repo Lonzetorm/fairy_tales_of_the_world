1c2237a086b814150bb1b5ee29402f9f0e7ab4d8
Азаров Владимир <azarov@solid.com.ru>
2024-08-05 23:08:24 +0300

    Вот вся эта хуйня тупая

--- server/api/auth/register.ts
+++ server/api/auth/register.ts
@@ -5,13 +5,11 @@ import { ZodError } from "zod"
 import sendDefaultErrorResponse from '@/server/app/errors/responses/DefaultErrorsResponse';
 import registerRequest from '@/server/app/formRequests/RegisterRequest';
 import { validateUser } from '@/server/app/services/userService';
-// import { makeSession } from '~/server/app/services/sessionService';
+import { makeSession } from '~/server/app/services/sessionService';
 import sendZodErrorResponse from '@/server/app/errors/responses/ZodErrorsResponse';
 
 export default eventHandler(async (event: H3Event) => {
   try {
-      let req = await readBody(event)
-
     const data = await registerRequest(event)
     const validation = await validateUser(data)
 
@@ -20,11 +18,11 @@ export default eventHandler(async (event: H3Event) => {
       return sendError(event, createError({ statusCode: 422, data: errors }))
     }
 
-    const encryptedPassword: string = await bcrypt.hash(req.password, 10)
+    const encryptedPassword: string = await bcrypt.hash(data.password, 10)
 
     const user = await createUser(data.name, data.email, encryptedPassword)
 
-    // return await makeSession(user, event)
+    return await makeSession(user, event)
   } catch (error: any) {
     if (error.data instanceof ZodError) {
       return await sendZodErrorResponse(event, error.data)
--- server/app/services/userService.ts
+++ server/app/services/userService.ts
@@ -1,9 +1,10 @@
 import { RegistrationRequest } from '~~/types/IRegistration';
 import { H3Event } from 'h3';
 import { getUserBySessionToken } from './sessionService';
-import { isString } from '@vueuse/core';
+// import { isString } from '@vueuse/core';
 import { IUser } from '~~/types/IUser';
 import { validate } from './validator';
+import {useState} from "nuxt/app";
 
 export async function validateUser(data: RegistrationRequest) {
 
@@ -23,29 +24,31 @@ export function sanitizeUserForFrontend(user: IUser | undefined): User | undefin
         return user
     }
 
-    delete user.password;
+    // delete user.password;
 
     return user as User
 }
 
 export async function authCheck(event: H3Event): Promise<boolean> {
 
-    const authToken = getCookie(event, 'auth_token') 
-    
-    const hasAuthToken = isString(authToken)
+    const authToken = getCookie(event, 'auth_token')
+    console.log('authToken 1: ', authToken)
+
+    // const hasAuthToken = isString(authToken)
+    const hasAuthToken = (typeof authToken) === 'string'
 
     if(!hasAuthToken) {
         return false
     }
 
     const user  = await getUserBySessionToken(authToken)
+    // todo: Наличие здесь useSate - это костыль, который надо будет переделать
+    useState('user').value = authToken
 
-    if(user?.id) {
+    if(user) {
         return true
     }
 
     return false
-
-
 }
 
