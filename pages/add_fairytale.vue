fa905058c4d964ae0fc616c31633eb84e69c185c
Азаров Владимир <azarov@solid.com.ru>
2024-08-07 10:03:46 +0300

    Начало формы добавления сказки

---
+++ pages/add_fairytale.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="border border-solid border-gray-200 rounded-lg p-4 mb-4">
+    <h1 class="font-bold mb-3">
+      Добавить сказку
+    </h1>
+    <!--      <NuxtImg class="w-1/2 float-left mr-4 rounded-lg" format="webp" src="images/kolobok.jpg" alt="kolobok"/>-->
+    <p>
+
+    </p>
+  </div>
+  <div class="border border-solid border-gray-200 rounded-lg p-4">
+    <h1 class="font-bold mb-3">
+      Текст
+    </h1>
+    <!--      <NuxtImg class="w-1/2 float-left mr-4 rounded-lg" format="webp" src="images/kolobok.jpg" alt="kolobok"/>-->
+    <p>
+
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
