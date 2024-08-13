263fb45807692759e30f709b04653e4ed610ba2f
Азаров Владимир <azarov@solid.com.ru>
2024-08-13 10:20:32 +0300

    Изменена индексная страница peoples

--- pages/tales/[id].vue
+++ pages/tales/[id].vue
@@ -4,7 +4,6 @@
       <h1 class="font-bold mb-3 text-center">
         {{ tale.name }}
       </h1>
-<!--      <NuxtImg class="w-1/2 float-left mr-4 rounded-lg" format="webp" src="images/kolobok.jpg" alt="kolobok"/>-->
       <p v-html="tale.text"/>
     </div>
     <Comments/>
