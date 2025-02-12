b42d62178c31875c87d363608007b6216521159d
Азаров Владимир <azarov@solid.com.ru>
2025-02-12 20:03:36 +0300

    Правка сайдбара

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="w-100">
-    <div class="rounded-lg mb-4 bg-green-500 w-100 py-3 px-3 border-solid border-2 border-green-500 hover:border-white hover:cursor-pointer hover:text-white rounded">
-      <NuxtLink :to="'/peoples/'">
+    <div class="rounded-lg mb-4 bg-green-500 w-100 border-solid border-2 border-green-500 hover:border-white hover:cursor-pointer hover:text-white rounded">
+      <NuxtLink :to="'/peoples/'" class="w-100 block py-3 px-3">
         По народам
       </NuxtLink>
     </div>
     <ul class="rounded-lg bg-green-500 mb-4">
-      <li v-for="category in categories" class="rounded-lg py-3 px-3 border-solid border-2 border-green-500 hover:border-white hover:cursor-pointer hover:text-white rounded">
-        <NuxtLink :to="'/categories/' + category.code">
+      <li v-for="category in categories" class="rounded-lg border-solid border-2 border-green-500 hover:border-white hover:cursor-pointer hover:text-white rounded">
+        <NuxtLink :to="'/categories/' + category.code" class="w-100 block py-3 px-3">
           {{category.name }}
         </NuxtLink>
       </li>
