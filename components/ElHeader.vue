f1324f8e934b9fbab94bd12f4f778d4c3970f0d9
Азаров Владимир <azarov@solid.com.ru>
2025-10-11 20:58:37 +0300

    Изменение главной страницы

--- components/ElHeader.vue
+++ components/ElHeader.vue
@@ -1,7 +1,7 @@
 <template>
     <header class="border-b bg-green-500 px-4 md:px-0 md:px-[126px]">
       <div class="flex items-center justify-between h-12 text-white max-w-screen-xl mx-auto">
-        <MobileMenu v-if="store.mobileMenuShown"/>
+<!--        <MobileMenu v-if="store.mobileMenuShown"/>-->
         <NuxtLink class="dark:text-purple-500 font-bold text-logo-main" to="/">
           Все сказки
         </NuxtLink>
@@ -24,7 +24,14 @@
               v-if="useState('user').value !== undefined"
           />
           <SearchIcon @click="store.modalSearch = true" class="my-auto mr-4 cursor-pointer"/>
-          <MenuIcon @click="store.mobileMenuShown = true" class="my-auto"/>
+          <div class="relative">
+            <MenuIcon @click="toggleModalMenu" class="my-auto"/>
+            <block-menu
+                v-if="store.menuShown"
+                @click="toggleModalMenu"
+                class="absolute top-10 right-0 w-[200px]"
+            />
+          </div>
         </div>
       </div>
     </header>
@@ -38,5 +45,10 @@ const logout = userLogout
 
 //todo: Заменить проверку залогинен ли на использование isLoggedIn
 const isLoggedIn = await useLoggedIn()
+
+const toggleModalMenu = () => {
+  store.mobileMenuShown = !store.mobileMenuShown;
+  store.menuShown = !store.menuShown
+}
 
 </script>
---
+++ components/block/Menu.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="rounded-lg mb-4 bg-green-500">
+    <NuxtLink :to="'/peoples/'" class="rounded-lg w-100 block py-3 px-3 border-solid border-2 border-green-500 hover:border-white hover:cursor-pointer hover:text-white rounded">
+      По народам
+    </NuxtLink>
+    <NuxtLink :to="'/authors/'" class="rounded-lg w-100 block py-3 px-3 border-solid border-2 border-green-500 hover:border-white hover:cursor-pointer hover:text-white rounded">
+      Авторские сказки
+    </NuxtLink>
+    <li v-for="category in categories" class="rounded-lg border-solid border-2 border-green-500 hover:border-white hover:cursor-pointer hover:text-white rounded">
+      <NuxtLink :to="'/categories/' + category.code" class="w-100 block py-3 px-3">
+        {{category.name }}
+      </NuxtLink>
+    </li>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import {useFetch} from "nuxt/app";
+
+const { data: categories } = useFetch('/api/categories/')
+console.log('categories', categories)
+</script>
+
+<style scoped>
+
+</style>
